c617607c063ebd0723c022e1d636e792eedf54e3
zoldeks <bearallan16@gmail.com>
2024-07-22 19:15:24 -0300

    Initial commit

---
+++ pythonProject1/aula06a.py
@@ -0,0 +1,7 @@
+from this import s
+
+n1 = int(input('digite um valor: '))
+n2 = int(input('digite outro: '))
+s = n1 + n2
+#print('a soma vale entre' ,'6' 'e' ,'8' 'vale' ,'14
+print('a soma entre {} e {} vale {}'.format(n1, n2, s))
---
+++ pythonProject1/aula07b.py
@@ -0,0 +1,2 @@
+nome = input('qual e seu nome? ')
+print('prazer em te conhecer {:20}!'.format(nome))
